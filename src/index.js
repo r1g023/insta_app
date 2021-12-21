7f9b7b77990cf8deea351bb6f0cfb771d06b0ad4
rigo0523 <limon1285@gmail.com>
2021-12-20 23:10:45 -0600

    practice react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,94 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  const [developer, setDeveloper] = React.useState({
+    language: "python",
+    years: "",
+    isEmployed: false,
+    name: "",
+    title: "",
+  });
+
+  //effect callback, conditioanlly run this function only when title is changed so
+  //it only runs once when the app rerenders on state change
+  React.useEffect(() => {
+    //affect function
+    document.title = developer.title;
+    console.log("runs effect");
+  }, [developer.title]);
+
+  const handleChanges = (e) => {
+    setDeveloper({
+      ...developer,
+      language: "javascript",
+    });
+  };
+
+  const handleInput = (e) => {
+    console.log("e--->", e.target.name, ": ", e.target.value);
+    setDeveloper({
+      ...developer,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const employ = (e) => {
+    setDeveloper((prev) => {
+      return {
+        ...prev,
+        isEmployed: !prev.isEmployed,
+      };
+    });
+  };
+
+  return (
+    <div>
+      {console.log("component did mount")}
+      <h3>test</h3>
+      <button onClick={handleChanges}>Change Language</button>
+      <label htmlFor="years">
+        Years:{" "}
+        <input
+          type="number"
+          name="years"
+          onChange={handleInput}
+          value={developer.years}
+        />
+      </label>
+      <label htmlFor="name">
+        Name:
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={handleInput}
+          value={developer.name}
+        />
+      </label>
+
+      <label htmlFor="title">
+        Title:
+        <input
+          type="text"
+          name="title"
+          id="title"
+          onChange={handleInput}
+          value={developer.title}
+        />
+      </label>
+
+      <button onClick={employ}>EMPLOYED</button>
+      <div className="one">
+        <p>Change language: {developer.language}</p>
+        <p>Change years: {developer.years}</p>
+        <p>Name input: {developer.name} </p>
+        <p>employment: {developer.isEmployed ? "employed" : "not Employed"}</p>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, root);
