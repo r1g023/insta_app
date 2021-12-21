0028d17eefa4a1f5d63fb3a3774342a358d502b3
rigo0523 <limon1285@gmail.com>
2021-12-21 16:12:13 -0600

    error validation

--- src/index.js
+++ src/index.js
@@ -1,44 +1,80 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
-
-const root = document.getElementById("root");
+import axios from "axios";
+const baseUrl = `https://api.github.com/users/`;
 
 function App() {
-  const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 });
+  const [user, setUser] = useState({ users: "rigo0523", errors: "" });
 
-  useEffect(() => {
-    window.addEventListener("mousemove", handleMouseMove);
-    console.log("mouse moved");
-    return () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-      console.log("remove mouse even listener");
-    };
-  }, []);
-
-  function handleMouseMove(e) {
-    setMousePosition({ x: e.pageX, y: e.pageY });
+  function getUsers() {
+    axios
+      .get(`${baseUrl}${user.users}`)
+      .then((response) => {
+        console.log(response.data);
+        setUser({
+          ...user,
+          users: response.data,
+          errors: "",
+        });
+      })
+      .catch((err) => {
+        console.log("error cant fetch user", err);
+        setUser({
+          ...user,
+          errors: `404 error, can't find user of ${user.users}`,
+        });
+      });
   }
 
+  function handleInput(e) {
+    e.persist();
+    console.log(e.target.value);
+    setUser({
+      ...user,
+      users: e.target.value,
+    });
+  }
+
+  useEffect(() => {
+    console.log("use effect hook");
+    getUsers();
+  }, []);
+
+  console.log("component did mount");
   return (
     <div>
-      {console.log("component did mount")}
-      <p>
-        X: {mousePosition.x}, Y: {mousePosition.y}
-      </p>
+      <input type="text" placeholder="find user" onChange={handleInput} />
+
+      <button onClick={getUsers}>Search user</button>
+      <button>Clearn input</button>
+      {user.errors && <p style={{ color: "red" }}>Error: {user.errors}</p>}
+      {!user.errors && user ? (
+        <div>
+          <h1>username: {user.users.login}</h1>
+          <img
+            src={user.users.avatar_url}
+            alt="git"
+            style={{ height: "100px" }}
+          />
+        </div>
+      ) : (
+        <h1>Error Loading...</h1>
+      )}
     </div>
   );
 }
 
+const root = document.getElementById("root");
 ReactDOM.render(<App />, root);
 
-function Test() {
-  console.log("component did mount TEST");
+// function Test() {
+//   console.log("component did mount TEST");
 
-  return (
-    <div className="one">
-      <h1>test</h1>
-    </div>
-  );
-}
+//   return (
+//     <div className="one">
+//       <h1>test</h1>
+//     </div>
+//   );
+// }
 
-setTimeout(() => ReactDOM.render(<Test />, root), 2000);
+// setTimeout(() => ReactDOM.render(<Test />, root), 2000);
