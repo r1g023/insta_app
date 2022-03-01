7fc53337918bff569146834c0fa8af9e43fef27b
rigo0523 <limon1285@gmail.com>
2022-02-28 23:13:37 -0600

    implemented Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+//modal to display createpost.jsx
+import react from "react";
 
-function Modal() {
+function Modal(modal) {
   return (
-    <div className="modal">
-      <h2>Create Post section!!</h2>
+    <div className="modal-container">
+      <div className="modal">
+        <div className="modal-header">
+          <h3>{modal.title}</h3>
+          <button className="modal-close-button" onClick={modal.onClose}>
+            <span>&times;</span>
+          </button>
+        </div>
+        <div className="modal-content">{modal.children}</div>
+      </div>
     </div>
   );
 }
-export default Modal;
