3e33fbc2ee09a46b93cf8b235f5cf42eab9586ad
rigo0523 <limon1285@gmail.com>
2022-04-13 23:16:20 -0500

    login component implemented

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,34 @@
 import React, { useState } from "react";
 
 function Login() {
-  return <div className="login"></div>;
+  const [username, setUsername] = useState("");
+
+  function handleChanges(e) {
+    console.log(e.target.name, e.target.value);
+    setUsername(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setUsername(username);
+  }
+
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Login:</label>
+        <input
+          type="text"
+          name="username"
+          onChange={handleChanges}
+          value={username}
+        />
+        <button>Login</button>
+      </form>
+      {username}
+    </div>
+  );
 }
 
 export default Login;
