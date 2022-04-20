863005ce4bbc14eccbe473184197b377933cfe33
rigo0523 <limon1285@gmail.com>
2022-04-19 23:12:48 -0500

    modal added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,36 @@ import React, { useState, useEffect } from "react";
 import Login from "./components/Login";
 import Header from "./components/Header";
 import CreatePost from "./components/CreatePost";
+import Modal from "./components/Modal";
 
 function App() {
   const [user, setUser] = useState("");
   const [posts, setPosts] = useState([]);
+  const [toggleModal, setToggleModal] = useState(false);
 
   useEffect(() => {
     document.title = user ? `welcome ${user}` : "Please login";
   }, [user]);
 
+  function handleToggleModal() {
+    setToggleModal(!toggleModal);
+  }
+
   if (!user) return <Login setUser={setUser} />;
   return (
     <div className="App">
       <Header user={user} signOut={() => setUser("")} />
-      <CreatePost setPosts={setPosts} posts={posts} user={user} />
+      <button onClick={handleToggleModal}>Open Modal</button>
+      {toggleModal ? (
+        <Modal onCancel={handleToggleModal}>
+          <CreatePost
+            setPosts={setPosts}
+            posts={posts}
+            user={user}
+            closeModal={handleToggleModal}
+          />
+        </Modal>
+      ) : null}
       <>
         {posts.map((item) => (
           <div className="postCard" key={item.id}>
