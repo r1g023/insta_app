9cd420d30df6d7ac9a726aa35e809ebed94929c8
rigo0523 <limon1285@gmail.com>
2022-04-27 23:13:24 -0500

    form validation added

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
+import * as yup from "yup";
 
 function CreatePost({ addAPost, user, toggleModalNow }) {
   const [newPost, setNewPost] = useState({
@@ -7,9 +8,48 @@ function CreatePost({ addAPost, user, toggleModalNow }) {
     completed: false,
   });
 
+  //errors for form validation
+  const [errors, setErrors] = useState(newPost);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
   const ref = useRef();
 
+  //Form validation error for inputs
+  let formSchema = yup.object().shape({
+    content: yup
+      .string()
+      .required("please enter content")
+      .min(3, "min of 3 characters needed on content input"),
+    image: yup.mixed().required("please upload a file"),
+  });
+
+  useEffect(() => {
+    formSchema.isValid(newPost).then((valid) => {
+      console.log("button enabled--->", valid);
+      setButtonDisabled(!valid);
+    });
+  }, [newPost]);
+  console.log("errors-...>", errors);
+
+  //validate for errors if inputs are not filled out completely based on yup
+  function ValidateChanges(e) {
+    //get by input name
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.type === "file" ? e.target.files[0] : e.target.value)
+      .then((valid) => {
+        console.log("valid, no errors---->", valid);
+        setErrors({ ...errors, [e.target.name]: "" });
+      })
+      .catch((err) => {
+        console.log("errors on yup validation---->", err.errors);
+        setErrors({ ...errors, [e.target.name]: err.errors[0] });
+      });
+  }
+
   function handleChanges(e) {
+    e.persist();
+    ValidateChanges(e);
     console.log(e.target.name, e.target.value, e.target.files);
     setNewPost({
       ...newPost,
@@ -37,17 +77,25 @@ function CreatePost({ addAPost, user, toggleModalNow }) {
     <div className="post">
       <h2>Create Post</h2>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="image">Image</label>
-        <input type="file" name="image" onChange={handleChanges} ref={ref} />
+        <label htmlFor="image">
+          Image
+          <input type="file" name="image" onChange={handleChanges} ref={ref} />
+          {errors.image ? <p style={{ color: "red" }}>{errors.image}</p> : null}
+        </label>
 
-        <label htmlFor="content">Content</label>
-        <input
-          type="text"
-          name="content"
-          value={newPost.content}
-          onChange={handleChanges}
-        />
-        <button>New Post</button>
+        <label htmlFor="content">
+          Content
+          <input
+            type="text"
+            name="content"
+            value={newPost.content}
+            onChange={handleChanges}
+          />
+          {errors.content ? (
+            <p style={{ color: "red" }}>{errors.content}</p>
+          ) : null}
+        </label>
+        <button disabled={buttonDisabled}>New Post</button>
       </form>
     </div>
   );
