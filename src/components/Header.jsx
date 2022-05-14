c159ab3c395c228572a3896e51660da0e16cee89
rigo0523 <limon1285@gmail.com>
2022-05-14 00:03:58 -0500

    implementing userContext with useReducer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../App";
 
-function Header({ user, signOut }) {
+function Header({ signOut }) {
+  const { user } = useContext(UserContext);
+  console.log("user--->", user);
   return (
     <div className="header">
       <h1>Header</h1>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,15 +1,13 @@
 import React, { useContext } from "react";
-
 import { UserContext } from "../App";
 
-export default function Post({ user, id, content, image, completed }) {
-  const currentUser = useContext(UserContext);
-  console.log("current user----->", currentUser);
-  const isCurrentUser = currentUser === user;
+export default function Post({ id, content, image, completed }) {
+  const { user } = useContext(UserContext);
+
   return (
     <div className="post-container">
       <div className={completed ? "completed" : ""}>
-        <p style={{ color: isCurrentUser && "green" }}>User: {user}</p>
+        <p style={{ color: user ? "green" : "" }}>User: {user}</p>
         <p>Content: {content}</p>
         {image && (
           <img src={URL.createObjectURL(image)} alt="randomIm" width="50px" />
