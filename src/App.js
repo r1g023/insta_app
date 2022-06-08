88c53aca6040acdbc3068f0e9f4a7af8e1618eaf
rigo0523 <limon1285@gmail.com>
2022-06-07 23:10:46 -0500

    reset form

--- src/App.js
+++ src/App.js
@@ -1,46 +1,7 @@
-import React, {
-  useState,
-  useEffect,
-  createContext,
-  useContext,
-  useReducer,
-} from "react";
-import Login from "./components/Login";
-import Header from "./components/Header";
-import CreatePost from "./components/CreatePost";
-import PostList from "./components/PostList";
-import { initialState, postReducer } from "./reducers/reducers";
-
-export const UserContext = createContext();
-export const PostContext = createContext();
+import React, { useState } from "react";
 
 function App() {
-  const [user, setUser] = useState("");
-  const [state, dispatch] = useReducer(postReducer, initialState);
-
-  useEffect(() => {
-    document.title = user ? `welcome ${user}` : "Please login";
-  }, [user]);
-
-  if (!user) return <Login setUser={setUser} />;
-  return (
-    <PostContext.Provider value={{ state, dispatch }}>
-      <UserContext.Provider value={{ user, setUser }}>
-        <div className="App">
-          <Header />
-
-          <CreatePost user={user} />
-
-          <div className="post-wrapper">
-            <PostList posts={state.posts} />
-          </div>
-          <button onClick={() => dispatch({ type: "DELETE_TOGGLE_SELECTED" })}>
-            DELETE TOGGLE SELECTED
-          </button>
-        </div>
-      </UserContext.Provider>
-    </PostContext.Provider>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
