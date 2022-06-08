29c8f39cc21918509221a033a099fe37db3bbc91
rigo0523 <limon1285@gmail.com>
2022-06-07 23:05:01 -0500

    reset forr graphql

--- src/components/CreatePost.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-function CreatePost() {
-  return <div className="post"></div>;
-}
-
-export default CreatePost;
--- src/components/Login.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-function Login() {
-  return <div className="login"></div>;
-}
-
-export default Login;
