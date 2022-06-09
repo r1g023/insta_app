279ea1e1ca4c3dc9ee967356a9a0dc1c24aeb713
rigo0523 <limon1285@gmail.com>
2022-06-09 17:20:35 -0500

    full CRUD with cache implementation

--- src/App.js
+++ src/App.js
@@ -23,9 +23,38 @@ const TOGGLE_TODO = gql`
   }
 `;
 
+const ADD_TODO = gql`
+  mutation addTodo($text: String!) {
+    insert_todos(objects: { text: $text }) {
+      returning {
+        id
+        text
+        done
+      }
+    }
+  }
+`;
+
+const DELETE_TODO = gql`
+  mutation deleteTodo($id: uuid!) {
+    delete_todos(where: { id: { _eq: $id } }) {
+      returning {
+        id
+        text
+        done
+      }
+    }
+  }
+`;
+
 function App() {
+  const [myText, setMyText] = useState("");
   const { loading, error, data } = useQuery(GET_TODOS);
   const [toggleTodo] = useMutation(TOGGLE_TODO);
+  const [addTodo] = useMutation(ADD_TODO, {
+    onCompleted: () => setMyText(""),
+  });
+  const [deleteTodo] = useMutation(DELETE_TODO);
 
   async function handleToggle(todo) {
     let data = await toggleTodo({
@@ -33,6 +62,40 @@ function App() {
     });
     console.log("toggle todo--->", data);
     return data;
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (!myText.trim()) return null;
+    let data = await addTodo({
+      variables: { text: myText },
+      refetchQueries: [{ query: GET_TODOS }],
+    });
+    console.log("add todo--->", data);
+    return data;
+  }
+
+  async function handleDelete(todo) {
+    if (window.confirm("Are you sure you want to delete this todo?")) {
+      let data = await deleteTodo({
+        variables: { id: todo.id },
+        //UPDATE gives us direct access to the cached data
+        update: (cache) => {
+          console.log("cache--->", cache);
+          // prevData - all data before mutation is performed (or deleted)
+          const prevData = cache.readQuery({ query: GET_TODOS });
+          //now that it's deleted we need to remove it from the cache
+          const newTodos = prevData.todos.filter((item) => {
+            console.log("item delete-------->", item);
+            return item.id !== todo.id;
+          });
+          console.log("newTodos--->", newTodos);
+          cache.writeQuery({ query: GET_TODOS, data: { todos: newTodos } });
+        },
+      });
+      console.log("delete todo--->", data);
+      return data;
+    }
   }
 
   console.log("data-->", data);
@@ -47,8 +110,14 @@ function App() {
         </span>
       </h1>
 
-      <form onSubmit={null} className="mb3">
-        <input type="text" className="pa2 f4 b--dashed" />
+      <form onSubmit={handleSubmit} className="mb3">
+        <input
+          type="text"
+          className="pa2 f4 b--dashed"
+          name="myText"
+          value={myText}
+          onChange={(e) => setMyText(e.target.value)}
+        />
         <button className="pa2 f4 bg-green">Add Todo</button>
       </form>
 
@@ -58,13 +127,17 @@ function App() {
           <p key={item.id} onDoubleClick={() => handleToggle(item)}>
             <span className={`pointer list pa1 f3 ${item.done && "strike"}`}>
               Text: {item.text}
-              <button className="bg-transparent f4 bn">
+              <button
+                className="bg-transparent f4 bn"
+                onClick={() => handleDelete(item)}
+              >
                 <span className="red f2">&times;</span>
               </button>
             </span>
           </p>
         ))}
       </div>
+      <p>MY TEXT: {myText}</p>
     </div>
   );
 }
