6937ed903d8a81b659d72636322ab52db5e4b6e4
r1g023 <limon1285@gmail.com>
2023-07-26 16:29:11 -0500

    redo refresher

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React, { useState } from "react";
 
-import PostList from "./components/PostList";
-import CreatePost from "./components/CreatePost";
-
-// import axios, { Axios } from "axios";
-// const baseURL = "https://api.github.com/users/";
-// dummy post list
-const dummyPosts = [
-  {
-    id: 1,
-    title: "Post 1",
-    body: "Quisque cursus, metus vitae pharetra",
-    image: null,
-  },
-  {
-    id: 2,
-    title: "Post 2",
-    body: "Quisque cursus, metus vitae pharetra",
-    image: null,
-  },
-];
-
 function App() {
-  const [postList, setPostList] = useState(dummyPosts);
-  console.log("postList------>", postList);
   return (
     <div>
       {console.log("did render")}
       <h1>App</h1>
-      <CreatePost postList={postList} setPostList={setPostList} />
-      <PostList postList={postList} />
     </div>
   );
 }
