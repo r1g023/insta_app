18b0a856129a49ca6259be20cd590a92b96a41bd
r1g023 <limon1285@gmail.com>
2023-08-11 09:09:08 -0500

    remove components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-import React, { useState, useEffect } from "react";
-import Login from "./components/Login";
-import Header from "./components/Header";
-import CreatePost from "./components/CreatePost";
+import React from "react";
 
 function App() {
-  const [user, setUser] = useState("");
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    document.title = user ? `Welcome ${user}` : "please login";
-  }, [user]);
-
-  if (!user) return <Login setUser={setUser} />;
-
   return (
-    <div className="App">
-      App
-      <Header user={user} signOut={setUser} />
-      <CreatePost user={user} posts={posts} setPosts={setPosts} />
-      <>
-        {posts.map((item) => (
-          <div className="post" key={item.id}>
-            {console.log("item---->", item)}
-            <p>Content: {item.content}</p>
-            <p>User: {item.user}</p>
-            {item.image && (
-              <img
-                src={URL.createObjectURL(item.image)}
-                alt="post"
-                height="50"
-              />
-            )}
-          </div>
-        ))}
-      </>
+    <div>
+      <h1>App</h1>
     </div>
   );
 }
