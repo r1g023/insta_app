70835deac08dcf1d39dd85668a52aebc3ac8db0f
r1g023 <limon1285@gmail.com>
2023-08-11 09:17:01 -0500

    force app changes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,9 @@
-import React, { useState } from "react";
-import Login from "./components/Login";
-import Logout from "./components/Logout";
-import PostList from "./components/PostList";
-import CreatePost from "./components/CreatePost";
+import React from "react";
 
 function App() {
-  const [user, setUser] = useState("");
-  const [postList, setPostList] = useState([
-    {
-      id: 1,
-      title: "My first post",
-      body: "This is my first post",
-      user: user,
-      image: null,
-    },
-  ]);
-
-  React.useEffect(() => {
-    const data = localStorage.getItem("user");
-
-    data ? setUser(JSON.parse(data)) : setUser("");
-  }, []);
-
-  React.useEffect(() => {
-    document.title = user ? `${user}'s Feed` : "Please login";
-
-    localStorage.setItem("user", JSON.stringify(user));
-  }, [user]);
-
-  if (!user) {
-    return <Login setUser={setUser} />;
-  }
-
   return (
     <div>
-      {user && <h2>Welcome, {user}</h2>}
-      <Logout user={user} setUser={setUser} />
-      {console.log("did render", user)}
-
-      <CreatePost postList={postList} setPostList={setPostList} />
-      <PostList postList={postList} />
+      <h1>React Hooks</h1>
     </div>
   );
 }
