5f21460da3b83e94a7e754dc3533d7ce30fb882b
r1g023 <limon1285@gmail.com>
2024-03-01 15:51:10 -0600

    useRef

--- src/index.js
+++ src/index.js
@@ -1,49 +1,60 @@
 import ReactDom from "react-dom";
 import React from "react";
 
+const baseUrl = "https://api.github.com/users";
+
 function App() {
-  const [dev, setDev] = React.useState({
-    language: "python",
-    yearsExperience: 3,
-    developer: true,
-  });
+  const [username, setUsernames] = React.useState("r1g023");
+  const [users, setUsers] = React.useState(null);
+  const inputRef = React.useRef(null);
+
+  function fetchUser() {
+    fetch(`${baseUrl}/${username}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }
+
+  React.useEffect(() => {
+    console.log("useEffect");
+    fetchUser();
+  }, []);
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          setDev({
-            ...dev,
-            language: "javascript",
-          });
+    <div style={{ padding: "30px" }}>
+      {console.log("render")}
+      <form
+        type="submit"
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetchUser();
         }}>
-        Change
-      </button>
-      <input
-        type="number"
-        name="yearsExperience"
-        onChange={(e) => {
-          console.log(e.target.value);
-
-          setDev({
-            ...dev,
-            [e.target.name]: e.target.value,
-          });
-        }}
-      />
-      <h1>Language: {dev.language}</h1>
-      <h1>Years of Experience: {dev.yearsExperience}</h1>
-
-      <button
-        onClick={() => {
-          setDev({
-            ...dev,
-            developer: !dev.developer,
-          });
-        }}>
-        Is Dev?
-      </button>
-      <h1>Developer: {dev.developer ? "Yes" : "No"}</h1>
+        <input
+          type="text"
+          ref={inputRef}
+          placeholder="Enter username"
+          onChange={(e) => {
+            setUsernames(e.target.value);
+          }}
+        />
+        <button>Search User</button>
+        <button
+          type="button"
+          onClick={() => {
+            inputRef.current.value = "";
+          }}>
+          Clear
+        </button>
+      </form>
+      {users && (
+        <div>
+          <h1>{users.login}</h1>
+          <img src={users.avatar_url} alt="user" style={{ height: "100px" }} />
+          <p>{users.bio}</p>
+        </div>
+      )}
+      Username: {username}
     </div>
   );
 }
