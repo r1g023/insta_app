4a5e58833cd491bbd37e8fa17499074ca2420f4a
r1g023 <limon1285@gmail.com>
2024-03-04 22:53:09 -0600

    catchError

--- src/index.js
+++ src/index.js
@@ -4,57 +4,78 @@ import React from "react";
 const baseUrl = "https://api.github.com/users";
 
 function App() {
-  const [username, setUsernames] = React.useState("r1g023");
-  const [users, setUsers] = React.useState(null);
-  const inputRef = React.useRef(null);
+  const [username, setUsername] = React.useState("r1g023");
+  const [user, setUser] = React.useState(null);
+  const clearInput = React.useRef();
+  const [userError, setUserError] = React.useState(null);
 
-  function fetchUser() {
+  function getUser() {
     fetch(`${baseUrl}/${username}`)
       .then((res) => res.json())
       .then((data) => {
-        setUsers(data);
+        console.log(data);
+        setUser(data);
+      })
+      .catch((error) => {
+        console.log("error---->", error.message);
+        setUserError(error);
       });
   }
 
   React.useEffect(() => {
     console.log("useEffect");
-    fetchUser();
+    getUser();
   }, []);
 
+  console.log("user outside", user);
+
   return (
-    <div style={{ padding: "30px" }}>
+    <div style={{ padding: "100px" }}>
       {console.log("render")}
+      {console.log("user inside", user)}
+      <h1>Find git user</h1>
       <form
-        type="submit"
         onSubmit={(e) => {
           e.preventDefault();
-          fetchUser();
+          getUser();
         }}>
         <input
           type="text"
-          ref={inputRef}
-          placeholder="Enter username"
-          onChange={(e) => {
-            setUsernames(e.target.value);
-          }}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          ref={clearInput}
         />
-        <button>Search User</button>
-        <button
-          type="button"
-          onClick={() => {
-            inputRef.current.value = "";
-          }}>
-          Clear
-        </button>
+        <button>Find User</button>
       </form>
-      {users && (
+      <button
+        onClick={() => {
+          clearInput.current.value = "";
+          setUser(null);
+        }}>
+        Clear input
+      </button>
+      {user && !user.message ? (
         <div>
-          <h1>{users.login}</h1>
-          <img src={users.avatar_url} alt="user" style={{ height: "100px" }} />
-          <p>{users.bio}</p>
+          <p>User: {user.login}</p>
+          <p>Followers: {user.followers}</p>
+          <p>Bio: {user.bio} </p>
+          <img
+            src={user.avatar_url}
+            alt={user.login}
+            style={{ height: "100px" }}
+          />
+          {userError && <p style={{ color: "red" }}>User not found</p>}
         </div>
+      ) : (
+        <p>Loading...</p>
       )}
-      Username: {username}
+
+      {user && user.message && (
+        <p style={{ color: "red" }}>
+          {user.login} {user.message}
+        </p>
+      )}
+      <p>Username: {username} </p>
     </div>
   );
 }
