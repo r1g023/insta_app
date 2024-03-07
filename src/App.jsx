2ec2c1435253d9c6f8624cb85773f6a69fe22e5b
r1g023 <limon1285@gmail.com>
2024-03-06 23:18:29 -0600

    restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React from "react";
-import Login from "./components/Login";
-import Header from "./components/Header";
 
 function App() {
-  const [user, setUser] = React.useState("");
-
-  React.useEffect(() => {
-    window.document.title = `Welcome, ${user}`;
-  }, [user]);
-
-  if (!user) {
-    return <Login setUser={setUser} />;
-  }
-
+  console.log("outside App");
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <Header user={user} setUser={setUser} />
+      <h1>App</h1>
     </div>
   );
 }
