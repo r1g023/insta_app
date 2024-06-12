18d95123e558e5449e24d75e60461914de8a17ec
r1g023 <limon1285@gmail.com>
2024-06-11 23:11:31 -0500

    start app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import React from "react";
 import "./../src/App.css";
-import Login from "./components/Login";
-import Header from "./components/Header";
-import dreamPost from "../src/postListItems";
-import PostList from "./components/PostList";
-import CreatePost from "./components/CreatePost";
 
 function App() {
-  const [user, setUser] = React.useState(null);
-  const [posts, setPosts] = React.useState(dreamPost);
-
-  React.useEffect(() => {
-    document.title = user ? `${user}'s Feed` : "Dream Feed";
-  }, [user]);
-
   return (
     <div style={{ border: "2px solid red", padding: "10px" }}>
-      <Header setUser={setUser} />
-      <CreatePost posts={posts} setPosts={setPosts} user={user} />
-      <PostList posts={posts} />
+      <p>Setup</p>
     </div>
   );
 }
