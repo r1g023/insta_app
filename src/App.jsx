38cb2c412cb2dd58d9d1a1fb694fd70ac1f895ff
r1g023 <limon1285@gmail.com>
2024-07-17 23:20:02 -0500

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./../src/App.css";
 
 function App() {
-  return <div style={{ border: "2px solid red", padding: "10px" }}></div>;
+  return (
+    <div style={{ border: "2px solid red", padding: "10px" }}>
+      <p>Setup</p>
+    </div>
+  );
 }
 
 export default App;
